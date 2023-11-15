433ed85aff3f0afe5880c7e72308b153375886f7
abdelhak <abdelhak.chenafa@univ-tlemcen.dz>
2023-11-15 13:50:28 -0800

    fix betty

--- FreeArray.c
+++ FreeArray.c
@@ -1,11 +1,6 @@
 #include "shell.h"
-<<<<<<< HEAD
-/*
- * freeStringArray- function that free an array of string
-=======
 /**
  * freeStringArray - function that free an array of string
->>>>>>> a38ce8a983f956c74a409ca7ce13b0262fe8d21c
  * @array: input
  * Return: nothing used for freeing memory
  */
